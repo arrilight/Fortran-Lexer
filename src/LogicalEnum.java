670101b8754cccb627d14d992de47fed0b1febb1
pavelkazancev02 <pavelkazancev02@gmail.com>
2018-09-22 15:52:02 +0300

    Maven support

--- src/LogicalEnum.java
+++
@@ -1,3 +0,0 @@
-public enum LogicalEnum {
-    AND, OR, NOT, NEQV, EQV, TRUE, FALSE, EQ, NE, LT, GT, LE, GE
-}
--- src/TokenType.java
+++
@@ -1,3 +0,0 @@
-public enum TokenType {
-    IDENTIFIER, KEYWORD, SEPARATOR, OPERATOR, LITERAL, LABEL
-}
